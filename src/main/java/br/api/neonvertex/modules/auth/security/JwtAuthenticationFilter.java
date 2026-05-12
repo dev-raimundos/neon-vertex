1bed615ae2d6b0d9f1ab605969c6a7efe8722a93
dev-raimundos <devraimundos@outlook.com>
2026-05-12 16:38:35 -0300

    Refatora `JwtAuthenticationFilter` para suportar autenticação via cookie e melhora formatação e legibilidade.

--- src/main/java/br/api/neonvertex/modules/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/br/api/neonvertex/modules/auth/security/JwtAuthenticationFilter.java
@@ -2,11 +2,6 @@ package br.api.neonvertex.modules.auth.security;
 
 import br.api.neonvertex.modules.auth.services.TokenService;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,7 +9,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
@@ -25,9 +25,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final CustomUserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         String token = extractToken(request);
 
@@ -36,14 +37,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 String subject = tokenService.validateTokenAndGetSubject(token);
 
                 if (subject != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
                     UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
-
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails,
+                    var authentication = new UsernamePasswordAuthenticationToken(userDetails,
                         null,
                         userDetails.getAuthorities());
-
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             } catch (Exception ignored) {
@@ -55,10 +52,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String extractToken(HttpServletRequest request) {
+
+        var cookie = WebUtils.getCookie(request, "access_token");
+        if (cookie != null) {
+            return cookie.getValue();
+        }
+
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             return authHeader.substring(7);
         }
+
         return null;
     }
 }
